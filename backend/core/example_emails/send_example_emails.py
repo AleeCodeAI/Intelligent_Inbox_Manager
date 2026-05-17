23f2bd0fe5c2eae0aa02843689985c9838852e9d
Ali Seena <aliwork0066@gmail.com>
2026-05-17 20:29:30 +0500

    feat: added three jsonl files with variety of example emails and a send_example_emails.py that sends example emails to our gmail

---
+++ backend/core/example_emails/send_example_emails.py
@@ -0,0 +1,83 @@
+import requests 
+from utils.color import Logger
+import logging
+from configs import MainSettings
+from pathlib import Path
+import json
+
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+
+class SendExampleEmails(Logger):
+    name: str = "SendExampleEmails"
+    color: str = Logger.ORANGE
+    
+    def __init__(self):
+        self.settings = MainSettings()
+        self.url = self.settings.SEND_EXAMPLE_EMAILS_N8N_URL
+        self.basic_emails_path = Path(__file__).parent / "basic_emails.jsonl"
+        self.priority_emails_path = Path(__file__).parent / "priority_emails.jsonl"
+        self.nonbusiness_emails_path = Path(__file__).parent / "nonbusiness_emails.jsonl"
+        self.log("Initialized SendExampleEmails")
+
+    def load_basic_emails(self):
+        basic_emails = []
+
+        with open(self.basic_emails_path, "r") as file:
+            for line in file:
+                basic_emails.append(json.loads(line))
+
+        return basic_emails[:10]
+    
+    def load_priority_emails(self):
+        priority_emails = []
+
+        with open(self.priority_emails_path, "r") as file:
+            for line in file:
+                priority_emails.append(json.loads(line))
+
+        return priority_emails[:5]
+
+    def load_nonbusiness_emails(self):
+        nonbusiness_emails = []
+
+        with open(self.nonbusiness_emails_path, "r") as file:
+            for line in file:
+                nonbusiness_emails.append(json.loads(line))
+
+        return nonbusiness_emails[:3]
+    
+    def load_all_emails(self):
+        all_emails = []
+
+        all_emails.extend(self.load_basic_emails())
+        all_emails.extend(self.load_priority_emails())
+        all_emails.extend(self.load_nonbusiness_emails())
+
+        return all_emails
+
+    def send_email(self, email: str):
+        
+        payload = {"email": email.dict()}
+        try:
+            response = requests.post(self.url, json=payload)
+            if response.status_code == 200:
+                self.log(f"Email sent successfully to {email.subject}")
+            else:
+                self.log(f"Failed to send email to {email.subject}. Status code: {response.status_code}")
+            return response
+        except Exception as e:
+            self.log(f"An error occurred while sending email to {email.subject}: {str(e)}")
+            return None
+
+if __name__ == "__main__":
+    from schemas import InboundEmail
+
+    sender = SendExampleEmails()
+
+    emails = sender.load_all_emails()
+
+    for email_data in emails:
+
+        email = InboundEmail(**email_data)
+
+        sender.send_email(email)
