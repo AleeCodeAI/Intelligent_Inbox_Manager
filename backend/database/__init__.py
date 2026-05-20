b1f7552de54a713deffbb7edbb606b7fc8768326
Ali Seena <aliwork0066@gmail.com>
2026-05-20 20:47:38 +0500

    feat: added the insert email functions  and update review functions

---
+++ backend/database/__init__.py
@@ -0,0 +1,5 @@
+from .repositories.basic_repo import insert_basic, mark_basic_reviewed
+from .repositories.email_repo import insert_email, get_email
+from .repositories.nonbusiness_repo import insert_nonbusiness, mark_nonbusiness_reviewed
+from .repositories.priority_repo import insert_priority, mark_priority_reviewed
+from .repositories.processing_repo import insert_processing
