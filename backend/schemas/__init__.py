eec5a5ebc30ce4338452819668e00fa594c445a6
Ali Seena <aliwork0066@gmail.com>
2026-05-20 23:03:22 +0500

    refactor: changed EmailResponse to BasicEmailResponse in __init__.py

--- backend/schemas/__init__.py
+++ backend/schemas/__init__.py
@@ -11,4 +11,4 @@ from .agentic_rag_evals_schemas import (
 )
 from .receive_email_schemas import InboundEmail, InboundEmailBatch
 from .executor import EmailProcessed
-from .basic_flow_schemas import AgentInput, EmailResponse
+from .basic_flow_schemas import AgentInput, BasicEmailResponse
