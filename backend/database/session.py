f3e593f6269c8742f6cd4ddd021ac93dc79b9b2f
Ali Seena <aliwork0066@gmail.com>
2026-05-21 00:10:52 +0500

    refactor: modified so it removed the unnecessary prints

--- backend/database/session.py
+++ backend/database/session.py
@@ -6,5 +6,4 @@ from configs import MainSettings
 settings = MainSettings()
 
 engine = create_engine(settings.POSTGRESQL_URL, echo=False)
-print(f"Connected to database: {settings.POSTGRESQL_URL}")
 SessionLocal = sessionmaker(bind=engine)
