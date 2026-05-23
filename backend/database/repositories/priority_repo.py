83ec3d8d3525a75ec7b43326aacd3d579703e3b8
Ali Seena <aliwork0066@gmail.com>
2026-05-23 16:36:46 +0500

    refactor: modified the file to match the model

--- backend/database/repositories/priority_repo.py
+++ backend/database/repositories/priority_repo.py
@@ -1,16 +1,21 @@
 from database.session import SessionLocal
 from database.models.priority import PriorityEmailData
+from schemas import PriorityResult
 
 
-def insert_priority(email_db_id: str, 
-                    priority_type: str, 
-                    reviewed: bool = False):
-    
+def insert_priority(
+    email_db_id: str,
+    data: PriorityResult,
+    reviewed: bool = False
+):
     db = SessionLocal()
+
     try:
         record = PriorityEmailData(
             email_db_id=email_db_id,
-            priority_type=priority_type,
+            priority_type=data.priority_type,
+            confidence=data.confidence,
+            reasoning=data.reasoning,
             reviewed=reviewed
         )
 
@@ -23,8 +28,16 @@ def insert_priority(email_db_id: str,
     finally:
         db.close()
 
-def mark_priority_reviewed(db, email_db_id: str):
-    db.query(PriorityEmailData)\
-      .filter(PriorityEmailData.email_db_id == email_db_id)\
-      .update({"reviewed": True})
-    db.commit()
+
+def mark_priority_reviewed(email_db_id: str):
+    db = SessionLocal()
+
+    try:
+        db.query(PriorityEmailData)\
+          .filter(PriorityEmailData.email_db_id == email_db_id)\
+          .update({"reviewed": True})
+
+        db.commit()
+
+    finally:
+        db.close()
