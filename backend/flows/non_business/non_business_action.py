45d426cebdb9ffa35cb87798e91673a2a4b99716
Ali Seena <aliwork0066@gmail.com>
2026-05-29 12:25:00 +0500

    feat: added the NonBusinessAction

---
+++ backend/flows/non_business/non_business_action.py
@@ -0,0 +1,43 @@
+from utils.send_email import send_to_n8n
+from utils.color import Logger
+from utils.template import render_email
+from schemas import NonBusinessAction as nonbusiness_action
+
+class NonBusinessAction(Logger):
+    name: str = "NonBusinessAction"
+    color: str = Logger.GOLD
+
+    def run(self, nonbusiness_action: nonbusiness_action):
+        self.log(f"Executing action for email ID: {nonbusiness_action.gmail_id} with non-business type: {nonbusiness_action.nonbusiness_type}")
+
+        try:
+            self.log(f"Rendering manual response for email ID: {nonbusiness_action.gmail_id}")
+            html_body = render_email(
+                recipient_name=nonbusiness_action.sender_name, # the email will be received by the sender, so we use their name as recipient_name
+                body=nonbusiness_action.manual_response
+                )
+
+            self.log(f"Sending manual response for email ID: {nonbusiness_action.gmail_id}")
+            result = send_to_n8n(
+                {
+                    "id": nonbusiness_action.gmail_id,
+                    "body": html_body,
+                    "email_type": "NONBUSINESS"
+                }
+            )
+            self.log(f"Manual response sent with status: {result['status']} for email ID: {result['emailId']}")
+            return result
+
+        except Exception as e:
+            self.log(f"Error rendering manual response for email ID: {nonbusiness_action.gmail_id} - {str(e)}")
+            return {"status": "error", "message": str(e)}
+
+if __name__ == "__main__":
+    action = NonBusinessAction()
+    test_action = nonbusiness_action(
+        gmail_id="19e65562885fc360",
+        sender_name="John Doe",
+        manual_response="I am currently unavailable. I will get back to you as soon as possible.",
+        nonbusiness_type="PERSONAL"
+    )
+    action.run(test_action)
