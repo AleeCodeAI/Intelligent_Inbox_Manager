5ad3a66482ff65a3143be85065e2897d72f65b11
Ali Seena <aliwork0066@gmail.com>
2026-05-29 12:32:15 +0500

    feat: added Appointment

--- backend/schemas/__init__.py
+++ backend/schemas/__init__.py
@@ -13,4 +13,5 @@ from .receive_email_schemas import InboundEmail, InboundEmailBatch
 from .executor import EmailProcessed, ExecutorResult
 from .basic_flow_schemas import BasicLLMInput, BasicEmailResponse
 from .priority_flow_schemas import PriorityResult, CalendarEventDetails, PriorityAction
-from .nonbusiness_flow_schemas import NonBusinessResult
+from .nonbusiness_flow_schemas import NonBusinessResult, NonBusinessAction
+from .appointment_schemas import Appointment
