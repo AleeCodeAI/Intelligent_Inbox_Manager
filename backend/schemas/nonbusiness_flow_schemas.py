e7713915da1f0d155346346567a12b687470b5fe
Ali Seena <aliwork0066@gmail.com>
2026-06-01 13:53:09 +0500

    fix: modifed the schemas for better vaidation

--- backend/schemas/nonbusiness_flow_schemas.py
+++ backend/schemas/nonbusiness_flow_schemas.py
@@ -1,12 +1,12 @@
 from pydantic import BaseModel, Field
-from typing import Optional
+from typing import Literal
 
 #============================================
 # non_business_flow.py
 #============================================
 class NonBusinessResult(BaseModel):
-    nonbusiness_type: str = Field(description="The category assigned to the email.")
-    confidence: Optional[float] = Field(description="The confidence score of the classification, if available.")
+    nonbusiness_type: Literal["PERSONAL", "PROMOTIONAL", "INFORMATIONAL", "SPAM"] = Field(description="The category assigned to the email.")
+    confidence: float = Field(ge=0.0, le=1.0, description="The confidence score of the classification, if available.")
     reasoning: str = Field(description="A concise explanation of why this classification was chosen based on the email content.")
 
 #============================================
@@ -16,4 +16,4 @@ class NonBusinessAction(BaseModel):
     gmail_id: str = Field(description="The unique identifier of the email in Gmail.")
     sender_name: str = Field(description="Name of the email sender.")
     manual_response: str = Field(description="The manual response to be sent to the sender.")
-    nonbusiness_type: str = Field(description="The non business category assigned to the email.")
+    nonbusiness_type: Literal["PERSONAL", "PROMOTIONAL", "INFORMATIONAL", "SPAM"] = Field(description="The non business category assigned to the email.")
