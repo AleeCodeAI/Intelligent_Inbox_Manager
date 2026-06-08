a27d11db54205958b71f9e1e14ff9bb9e1563e64
Ali Seena <aliwork0066@gmail.com>
2026-06-08 23:25:17 +0500

    feat: added schemas for deleting appointment

--- backend/schemas/appointment_schemas.py
+++ backend/schemas/appointment_schemas.py
@@ -1,13 +1,16 @@
 from pydantic import BaseModel, Field
-from typing import Optional
 
 class Appointment(BaseModel):
     email_db_id: str = Field(description="Foreign key linking to the associated email record.")
     
-    event_id: Optional[str] = Field(default=None)
+    event_id: str = Field(description="ID from generated calendar event")
     event_title: str = Field(description="Title of the calendar event.")
     event_start: str = Field(description="Start time of the calendar event.")
     event_end: str = Field(description="End time of the calendar event.")
 
     calendar_status: str = Field(description="Status of the calendar event (e.g., confirmed, error).")
     confirmation_email_status: str = Field(description="Status of the confirmation email (e.g., success, failed).")
+
+class DeleteAppointment(BaseModel):
+    gmail_id: str = Field(description="Foreign key linking to the associated email record.")
+    event_id: str = Field(description="ID from generated calendar event")
